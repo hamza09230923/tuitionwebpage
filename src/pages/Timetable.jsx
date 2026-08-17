ac5fefce258908fcc87600d930fd0d042c0b982e
K4ijo0 <fahadullah2003@gmail.com>
2026-08-18 00:59:25 +0100

    Revert "Refresh summer timetable deploy"
    This reverts commit 5ee5c91259507cd1d4adf4d04d34608672106bd1.

--- src/pages/Timetable.jsx
+++ src/pages/Timetable.jsx
@@ -171,7 +171,7 @@ function Timetable() {
 
   useEffect(() => {
     // Announce page load for screen readers
-    setAnnouncement('MySchola GCSE Timetable page loaded. Summer and September live lesson schedules available.')
+    setAnnouncement('MySchola GCSE Timetable page loaded. 6 days of live lessons available.')
   }, [])
 
   const handleDayClick = (dayData) => {
